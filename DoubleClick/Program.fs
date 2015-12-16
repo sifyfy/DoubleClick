dcd91b4265bbbc73726b75c790db9517e53c150f
siphilia <siphilia.rn@gmail.com>
2015-12-16 13:23:07 +0900

    Implement base features

---
+++ DoubleClick/Program.fs
@@ -0,0 +1,9 @@
+﻿
+let doubleClick (filePath: string) = ignore <| System.Diagnostics.Process.Start filePath
+
+[<EntryPoint>]
+let main argv =
+    if (Array.length argv >= 1)
+        then doubleClick <| Array.item 0 argv
+        else printfn "Usage: db FILE_PATH"
+    0 // 整数の終了コードを返します
